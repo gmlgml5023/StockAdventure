b782975a27b17f77ce3042efe63bec435e553ada
gahee <gmlgml5023@naver.com>
2024-11-21 15:11:31 +0900

    스켈레톤 코드 작성 - 프론트엔드

---
+++ frontend/src/router/index.js
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  // 라우트를 여기에 추가하세요
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+})
+
+export default router
