0aa7c7e2010b69fb2463ae41c6cbabe8c10aa4e1
gahee <gmlgml5023@naver.com>
2024-11-21 17:21:55 +0900

    feat: 커뮤니티 CRUD 기능 구현
    -Article 모델 정의 -Serializer 2개 정의 -dumpdata 생성
    
    resolves: #7

--- backend/StockAdventure/settings.py
+++ backend/StockAdventure/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -70,6 +71,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'StockAdventure.wsgi.application'
 
@@ -125,3 +127,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
